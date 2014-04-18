386dab4bb9c1509f60241692d9ca5c6033f977cc
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-18 11:03:59 -0400

    palindrome solution update

--- palindrome.js
+++ palindrome.js
@@ -1,14 +1,13 @@
 function removePunc(string){
-  var punc = [".","?","!",":",";","-","—","(",")","[","]",",","’","“","”","/"]
-  for (var i=0; i<punc.length; i++) {
-      string=string.replace(punc[i],'');
-    }
-    return string;
+  string=string.replace(/[^\w\s]/g,'');
+};
+
+function removeWhitespace(string){
+  string.replace(/\s/g,'');
 };
 
 function palindromeChecker(string){
-  string=removePunc(string.replace(/\s/g,'')).toLowerCase();
-
+  string= removeWhitespace(removePunc(string)).toLowerCase();
   if(string[0] === string[string.length-1]){
     var reverse = string.split("").reverse().join("");
     return (string === reverse) ? true : false;
@@ -17,10 +16,11 @@ function palindromeChecker(string){
 };
 
 palindromeChecker("racecar");
-palindromChecker("racecar!");
+palindromeChecker("racecar!");
 palindromeChecker("racecars");
 palindromeChecker("Sore was I ere I saw Eros.");
-palindromChecker("Noel sees Leon.");
+palindromeChecker("Noel sees Leon.");
 palindromeChecker("Draw O Caesar, erase a coward.");
 palindromeChecker("baby");
 palindromeChecker("babb");
+palindromeChecker("Draw O Caesar, erase a coward..");
