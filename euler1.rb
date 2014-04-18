c90004b8c41b216c5288d74e2ad0ce3cf652e7bd
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-18 10:40:25 -0400

    euler 1 in ruby

---
+++ euler1.rb
@@ -0,0 +1,7 @@
+#PROJECT EULER 1
+# Description: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Problem: Find the sum of all the multiples of 3 or 5 below 1000.
+
+puts (1...1000).select{|x| x%3 == 0 || x%5 == 0 }.reduce(:+)
+
+#Answer: 233168
